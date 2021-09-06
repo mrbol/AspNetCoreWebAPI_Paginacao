60d532d6a7f91e59871a286b9c411b3314727c2c
Waldiney Romero <wrf.romero@gmail.com>
2021-09-06 17:47:33 -0300

    Paginação no ASP.NET Core WebApi

---
+++ WebAPI/Services/IUriService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Filter;
+
+namespace WebAPI.Services
+{
+    public interface IUriService
+    {
+        public Uri GetPageUri(PaginationFilter filter, string route);
+    }
+}
